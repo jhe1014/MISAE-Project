aa36487e3fcf7b263e9867720a13ea491c802b23
jhe1014 <skana1014@naver.com>
2019-05-02 01:39:03 +0900

    Add Toolbar

--- app/src/main/java/com/project/misae_project/MainActivity.java
+++ app/src/main/java/com/project/misae_project/MainActivity.java
@@ -2,13 +2,23 @@ package com.project.misae_project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        setSupportActionBar(toolbar);
+        //setTitle("");
     }
 }
