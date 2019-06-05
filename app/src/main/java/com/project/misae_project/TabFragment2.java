b936e45a531d81df7b929ff1c4f61fbb5d7a44a8
jhe014 <jhe31240>
2019-06-05 11:21:34 +0900

    Connect NodeJS

--- app/src/main/java/com/project/misae_project/TabFragment2.java
+++ app/src/main/java/com/project/misae_project/TabFragment2.java
@@ -2,6 +2,7 @@ package com.project.misae_project;
 
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,6 +13,15 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -25,9 +35,26 @@ public class TabFragment2 extends Fragment {
 
     private LinearLayoutManager mLayoutManager;
 
+    private TextView weather_now;
+    private TextView weather_now_comment;
+    private TextView weather_max_temp;
+    private TextView weather_min_temp;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.tab_fragment2, container, false);
+
+        // 현재 날씨
+        weather_now = (TextView)rootview.findViewById(R.id.weather_text);
+        weather_now_comment = (TextView)rootview.findViewById(R.id.weather_comment);
+        weather_max_temp = (TextView)rootview.findViewById(R.id.max_temp);
+        weather_min_temp = (TextView)rootview.findViewById(R.id.min_temp);
+
+        try {
+            new JSONTask().execute("http://192.168.28.1:3000/weather_condition");
+        } catch (Exception e) {
+
+        }
 
         // 시간별 날씨 목록 (가로 RecyclerView)
         View view1 = inflater.inflate(R.layout.weather_time_item, container, false);
@@ -80,7 +107,83 @@ public class TabFragment2 extends Fragment {
 
         return rootview;
     }
+
+    // server.js 파일 실행부
+    public class JSONTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+                JSONObject jsonObject = new JSONObject();
+                //jsonObject.accumulate("user_id", "androidTest");
+                //jsonObject.accumulate("name", "yun");
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try{
+                    //URL url = new URL("http://192.168.25.16:3000/users");
+                    URL url = new URL(urls[0]);//url을 가져온다.
+                    con = (HttpURLConnection) url.openConnection();
+                    con.connect();//연결 수행
+
+                    //입력 스트림 생성
+                    InputStream stream = con.getInputStream();
+
+                    //속도를 향상시키고 부하를 줄이기 위한 버퍼를 선언한다.
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    //실제 데이터를 받는곳
+                    StringBuffer buffer = new StringBuffer();
+
+                    //line별 스트링을 받기 위한 temp 변수
+                    String line = "";
+
+                    //아래라인은 실제 reader에서 데이터를 가져오는 부분이다. 즉 node.js서버로부터 데이터를 가져온다.
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+
+                    //다 가져오면 String 형변환을 수행한다. 이유는 protected String doInBackground(String... urls) 니까
+                    return buffer.toString();
+
+                    //아래는 예외처리 부분이다.
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    //종료가 되면 disconnect메소드를 호출한다.
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        //버퍼를 닫아준다.
+                        if(reader != null){
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }//finally 부분
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        //doInBackground메소드가 끝나면 여기로 와서 텍스트뷰의 값을 바꿔준다.
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            weather_now.setText(result);
+        }
+
+    }
 }
+
 
 // 시간별 날씨 목록에서 사용하는 부분 (여기서부터)
 class WeatherTimeAdapter extends RecyclerView.Adapter<WeatherTimeViewHolder> {
