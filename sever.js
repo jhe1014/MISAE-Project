07e20699e54834c498358081b9d3a9525ce5df99
jhe1014 <skana1014@naver.com>
2019-06-09 03:06:20 +0900

    Connect MySQL to NodeJS

--- sever.js
+++ sever.js
@@ -1,38 +1,45 @@
-const express = require('express');
+var express = require('express');
+var mysql = require('mysql');
 
-const app = express();
-
-let weather_condition = [
-
-  {
-
-    id: 1,
-    condition: '맑음', 
-    comment : '오늘은 산책하기 좋은 날',
-    max_temp : 30,
-    min_temp : 18
-      
-  }
-
-]
+var connection = mysql.createConnection({
+    host : '192.168.8.1',
+    user : 'jhe',
+    password : 'jhe31240',
+    database : 'misae_db'
+});
 
 
+var app = express();
 
 
-//단순히 http://localhost:3000/users로 접근하면 users라는 객체를 json으로 response하라는 의미입니다.
+connection.connect(function(err){
+  if(!err) {  
+    console.log("Database is connected ... \n\n");    
+  } else {  
+   console.log("Error connecting database ... \n\n");    
+  }  
+});  
 
-app.get('/weather_condition', (req, res) => {
 
-   console.log("who get in here/weather_condition");
+app.get('/misae_db', function(req, res) {
 
-   res.json(weather_condition)
+   console.log("who get in here/misae_db");
 
+   connection.query('SELECT * from weather_now', function (err, row, fields) {
+   connection.end();
+       if (!err) {
+           //res.send(row);
+           console.log("The solution is : ", row);
+           res.json(row);
+       }
+       else
+           console.log("Error");
+   });
 });
 
 
 
-
-app.listen(3000, () => {
+app.listen(3000, function() {
 
   console.log("Example app listening on port 3000!");
 
